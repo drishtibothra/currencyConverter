bd340ca34fbefc1ce54ef05519ebe275b3253cb3
drishtibothra <drishti.bothra05@gmail.com>
2025-04-18 18:15:32 +0530

    Input box updated

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
     useEffect( () => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-            .then(response => response.json())
-            .then(data => {
-
-            })
-            .catch(error => {
-
+            .then( (response) => response.json() )
+            .then( (response) => setData(response[currency]) )
+            console.log(data)
+            .catch( (error) => {
+                console.error("Error fetching currency data:", error);
             });
-    }, [] )
+    }, [currency] )
+    console.log(data);
+    return data;
 }
+
+export default useCurrencyInfo;
