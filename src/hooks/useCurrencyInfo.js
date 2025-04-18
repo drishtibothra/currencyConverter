847797dd39dcf0ab0e9844fba7c36db1b5b7656f
drishtibothra <drishti.bothra05@gmail.com>
2025-04-18 17:02:05 +0530

    files uploaded

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    useEffect( () => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then(response => response.json())
+            .then(data => {
+
+            })
+            .catch(error => {
+
+            });
+    }, [] )
+}
